8526239dd0eb09ac48e7fc50b54cb9f870bf9be0
Gabriel Pereira <gabriel02ps@gmail.com>
2020-07-08 22:41:23 -0300

    Exercícios de Condições

--- aula12ex/ex014/script.js
+++ aula12ex/ex014/script.js
@@ -2,9 +2,9 @@ function carregar () {
     var msg = window.document.getElementById('msg')
     var img = window.document.getElementById('imagem')
     var data = new Date ()
-    //var hora = data.getHours()
-    var hora = 20;
-    msg.innerHTML = `Agora são ${hora} horas.`
+    var hora = data.getHours()
+    var min = data.getMinutes()
+    msg.innerHTML = `Agora são ${hora}:${min}.`
     if (hora >= 0 && hora < 12) {
         //BOM DIA
         img.src = 'fotomanha.png'
